910d19146d39ec5abea7bac10845eebb7b5564d1
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-06-06 06:02:35 +0700

    change

--- client/src/scenes/dashboard/index.js
+++ client/src/scenes/dashboard/index.js
@@ -60,7 +60,7 @@ const Dashboard = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Header title="BOARD" subtitle="Welcome to your dashboard" />
         <Box>
           <Button
             sx={{
